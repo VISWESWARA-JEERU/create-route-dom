184b11eaba3624d9279b057306b5dd4d3b282978
VISWESWARA-JEERU <jvsks28@gmail.com>
2025-11-25 23:21:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Header from './components/header.jsx'
+import Hero from './components/hero.jsx'
+import Footer from './components/footer.jsx'   
+import { Outlet } from 'react-router-dom';   
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { RouterProvider,createBrowserRouter  } from 'react-router-dom'
+import Feature from './components/feature.jsx'
+import Hero from './components/hero.jsx'
+import Blog from './components/blog.jsx'
+import Team from './components/team.jsx'
+import About from './components/about.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children :[
+      {
+        path:"",
+        element:<Hero />
+      },
+      {
+        path:"feature",
+        element:<Feature />
+      },{
+        path:"blog",
+        element :<Blog />
+      },{
+        path:"team",
+        element :<Team />
+      },{
+        path:"about",
+        element :<About />
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
